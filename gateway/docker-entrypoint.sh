80b4cb5966177e320a525598589dadd3d644f907
Firas Ben Nacib <bennacibfiras@gmail.com>
2026-03-10 00:59:08 +0100

    Update springboot-angular container security workflow and gateway runtime defaults

--- gateway/docker-entrypoint.sh
+++ gateway/docker-entrypoint.sh
@@ -1,6 +1,15 @@
 #!/bin/sh
 set -eu
 
+: "${AUTH_SERVICE_UPSTREAM:=auth-service:8081}"
+: "${API_SERVICE_UPSTREAM:=api-service:8082}"
+: "${AUDIT_SERVICE_UPSTREAM:=audit-service:8084}"
+: "${NOTIFICATION_SERVICE_UPSTREAM:=notification-service:8090}"
+: "${FRONTEND_UPSTREAM:=}"
+
+export AUTH_SERVICE_UPSTREAM API_SERVICE_UPSTREAM AUDIT_SERVICE_UPSTREAM \
+  NOTIFICATION_SERVICE_UPSTREAM FRONTEND_UPSTREAM
+
 cp /etc/nginx/nginx.conf /tmp/nginx.conf
 sed -i 's|include /etc/nginx/conf.d/\*.conf;|include /tmp/default.conf;|' /tmp/nginx.conf
 
